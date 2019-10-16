6ed08153b7914807b2dffd9047b62a80ca170432
kzx616098 <kzx616098@163.com>
2019-10-16 11:36:47 +0800

    第一次

---
+++ src/Person/Hello.java
@@ -0,0 +1,22 @@
+package Person;
+
+public class Hello {
+    private String a;
+    private  String pass;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
